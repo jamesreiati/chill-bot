c5a9714b002854b8a46e282b4748e6f5fc148a93
Gage Ames <GageAmes@users.noreply.github.com>
2021-02-24 20:26:06 -0800

    Use .NET Generic Host and move config to JSON

---
+++ HardCoded/Config.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+
+namespace Reiati.ChillBot.HardCoded
+{
+    /// <summary>
+    /// Hard coded values related to configuration.
+    /// </summary>
+    public static class Config
+    {
+        /// <summary>
+        /// The prefix that can be used on environment variables to override configuration values.
+        /// </summary>
+        public const string EnvironmentVariablePrefix = "CHILLBOT_";
+
+        /// <summary>
+        /// The configuration key of the Discord token setting.
+        /// </summary>
+        public const string DiscordTokenConfigKey = "DiscordToken";
+
+        /// <summary>
+        /// The path to the default config file.
+        /// </summary>
+        public static readonly string DefaultConfigFilePath = Path.Combine(".", "config.json");
+
+        /// <summary>
+        /// The path to the local config file.
+        /// </summary>
+        public static readonly string LocalConfigFilePath = Path.Combine(".", "config.Local.json");
+    }
+}
